b35d18d472578fa4db5415f2d48e453af516e9f5
ashb155 <b.ashutosh198@gmail.com>
2025-08-25 01:08:10 +0530

    added capture logic into parser

--- parser.py
+++ parser.py
@@ -8,16 +8,28 @@ def parser(move,game):
         piece_type = pieces[move[0]]
         move=move[1:]
 
+    #capture marker
+    capture= "x" in move
+    move=move.replace("x","x")
+
+    #destination square
     destsq=move[-2:]
     dest=notation_to_index(destsq)
+
+    file=None
+    if piece_type=='P' and capture:
+        file=move[0]
 
     candidates = []
     for r in range(8):
         for c in range(8):
             piece = game.board[r][c]
             if piece and piece.colour == game.turn and piece.name == piece_type:
-                if dest in game.get_moves(r,c):
-                    candidates.append((r,c))
+                if dest in game.get_moves(r, c):
+                    if file:
+                        if index_to_notation((r, c))[0] != file:
+                            continue
+                    candidates.append((r, c))
 
     if len(candidates) == 1:
         return candidates[0], dest
